53027c1d9e1db67a6a9a8371bb862ebb67622b9e
Fanny08 <35570798+Fanny08@users.noreply.github.com>
2018-05-24 17:27:38 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="es">
 
 <head>
-<title>Titulo de la web</title>
+<title>Titilo Formulario</title>
 <meta charset="utf-8" />
 <link rel="stylesheet" href="estilos.css" />
 <link rel="shortcut icon" href="/favicon.ico" />
@@ -12,21 +12,22 @@
 
 <body>
     <header>
-       <h1>Mi sitio web</h1>
-       <p>Mi sitio web creado en html5</p>
+       <h1>Creando formulario</h1>
+       <p>Llena los datos</p>
     </header>
     <section>
-       <article>
-           <h2>Titilo de contenido<h2>
-           <p> Contenido (ademas de imagenes, citas, videos etc.) </p>
-       </article>
+		<form name="formulario" id= "formulario" action="recepto.php" method="post">
+		   <br><label for="nom">Nombre: </label>
+		   <input type="text" id="nom" name="nom"/></br>
+		   
+		   <br><label for="correo"> Correo: </label>
+		   <input type="email" id="correo" name="correo"/></br>
+		   
+		   <br><label for="telefono"> Telefono: </label>
+		   <input type="text" id="telefono"name="telefono"/></br>
+		   
+		   <input type="submit"  value="Enviar">
+		</form>
     </section>
-    <aside>
-       <h3>Titulo de contenido</h3>
-           <p>contenido</p>
-    </aside>
-    <footer>
-        Creado por mi el 2011
-    </footer>
 </body>
 </html>
---
+++ recepto.php
@@ -0,0 +1,13 @@
+<?php
+  $nombre =$_POST['nom'];
+  $correo =$_POST['correo'];
+  $tel =$_POST['telefono'];
+  
+  echo "Hola tu nombre es: $nombre <br>";
+  echo "tu correo es: $correo <br>";
+  echo "tu telefono es: $tel <br>";
+  
+  echo "<a href='index.php'> atras </a>";
+
+
+?>
